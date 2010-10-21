5db4d25b3de4113b3e8e7a02ab21243cda006728
Francis M. Tyers <ftyers@prompsit.com>
2010-10-21 09:25:26 +0000

    new testvoc

--- dev/testvoc.sh
+++ dev/testvoc.sh
@@ -2,4 +2,4 @@
 #sh inconsistency.sh ca-it > /tmp/ca-it.testvoc; sh inconsistency-summary.sh /tmp/ca-it.testvoc ca-it
 #echo ""
 echo "==Italian->Catalan=============================";
-sh inconsistency.sh it-ca > /tmp/it-ca.testvoc; sh inconsistency-summary.sh /tmp/it-ca.testvoc it-ca
+bash inconsistency.sh it-ca > /tmp/it-ca.testvoc; bash inconsistency-summary.sh /tmp/it-ca.testvoc it-ca
